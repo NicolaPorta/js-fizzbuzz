5e88853fbe419953eb40718007d496f1d8cd8cca
Nicola <nicola.porta00@gmail.com>
2020-11-17 13:56:27 +0100

    List Commit

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
+var array = [];
 for (var i = 1; i < 101; i++) {
   if (i%3 == 0) {
 
-    if (i%5 == 0) console.log("FizzBuzz");
+    if (i%5 == 0) array[i-1] = ("FizzBuzz");
 
-    else  console.log("Fizz");
+    else  array[i-1] = ("Fizz");
 
-  } else if (i%5 == 0) console.log("Buzz");
+  } else if (i%5 == 0) array[i-1] = ("Buzz");
 
-  else console.log(i);
+  else array[i-1] = i;
+}
+
+var stamp = document.getElementById('list');
+
+for (var i = 0; i < 100; i++) {
+  stamp.innerHTML += "<li>" + array[i] + "</li>";
 }
