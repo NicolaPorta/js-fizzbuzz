60f33c1f857d8f489f3b4df8e6f4c29b25a69f2c
Nicola <nicola.porta00@gmail.com>
2020-11-17 14:03:23 +0100

    Reduction js

--- js/script.js
+++ js/script.js
@@ -12,8 +12,6 @@ for (var i = 1; i < 101; i++) {
   else array[i-1] = i;
 }
 
-var stamp = document.getElementById('list');
-
 for (var i = 0; i < 100; i++) {
-  stamp.innerHTML += "<li>" + array[i] + "</li>";
+  document.getElementById('list').innerHTML += "<li>" + array[i] + "</li>";
 }
