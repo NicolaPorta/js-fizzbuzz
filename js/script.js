75d4a29462f25fdf191c4b338d1de64e463b46fd
Nicola <nicola.porta00@gmail.com>
2020-11-17 14:49:53 +0100

    Add style and functions

--- js/script.js
+++ js/script.js
@@ -1,17 +1,23 @@
-var array = [];
-for (var i = 1; i < 101; i++) {
+document.getElementById('btn').addEventListener("click",
+function() {
+  var array = [];
+  for (var i = 1; i < 101; i++) {
 
-  if (i%3 == 0) {
+    if (i%3 == 0) {
 
-    if (i%5 == 0) array[i-1] = ("FizzBuzz");
+      if (i%5 == 0) array[i-1] = ("FizzBuzz");
 
-    else  array[i-1] = ("Fizz");
+      else  array[i-1] = ("Fizz");
 
-  } else if (i%5 == 0) array[i-1] = ("Buzz");
+    } else if (i%5 == 0) array[i-1] = ("Buzz");
 
-  else array[i-1] = i;
-}
+    else array[i-1] = i;
+  }
 
-for (var i = 0; i < 100; i++) {
-  document.getElementById('list').innerHTML += "<li>" + array[i] + "</li>";
-}
+  for (var i = 0; i < 100; i++) {
+    document.getElementById('list').innerHTML += "<li>" + array[i] + "</li>";
+  }
+
+  document.getElementById('btn').className = "hidden";
+  document.getElementById('list').className = "show";
+})
